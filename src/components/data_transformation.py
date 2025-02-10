ad6966b52e0fe95633781a64cd9b0d9a4903f0f0
ManjulBalayar <90213041+ManjulBalayar@users.noreply.github.com>
2025-02-09 22:48:45 -0600

    Model Trainer complete

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -103,7 +103,6 @@ class DataTransformation:
             save_object(
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
-
             )
 
             return (
